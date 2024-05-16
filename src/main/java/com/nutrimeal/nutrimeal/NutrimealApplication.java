06da749ac6a73b53de77ebde49654b77ca453347
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-16 17:10:51 +0700

    project init

---
+++ src/main/java/com/nutrimeal/nutrimeal/NutrimealApplication.java
@@ -0,0 +1,13 @@
+package com.nutrimeal.nutrimeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrimealApplication.class, args);
+    }
+
+}
