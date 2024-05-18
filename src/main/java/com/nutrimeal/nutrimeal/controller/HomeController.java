f14b37c3dff1ed5bdd72c4627daada578c2c749e
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-18 19:53:12 +0700

    add bootstrap jquery

---
+++ src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nutrimeal.nutrimeal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/", "/home"})
+    public String home() {
+        return "home";
+    }
+}
