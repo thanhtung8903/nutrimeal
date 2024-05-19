11635e2252699007135cb219a2d2aa34e6cf6cf2
GIA KHANH <giakhanh27403@gmail.com>
2024-05-19 22:09:58 +0700

    update

--- src/main/java/com/nutrimeal/nutrimeal/controller/AuthController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/AuthController.java
@@ -4,13 +4,11 @@ import com.nutrimeal.nutrimeal.dto.request.SignupRequest;
 import com.nutrimeal.nutrimeal.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/api/v1/auth")
