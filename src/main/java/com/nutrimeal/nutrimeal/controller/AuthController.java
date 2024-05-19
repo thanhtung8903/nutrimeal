19b271fed5a903920b3c4bed72690abdcebcb908
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-05-19 20:08:45 +0700

    view: signup

---
+++ src/main/java/com/nutrimeal/nutrimeal/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.nutrimeal.nutrimeal.controller;
+
+import com.nutrimeal.nutrimeal.dto.request.SignupRequest;
+import com.nutrimeal.nutrimeal.service.UserService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Data
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String getSignupForm(Model model) {
+        if (!model.containsAttribute("SignupRequest")) {
+            model.addAttribute("SignupRequest", new SignupRequest());
+        }
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupUser(@ModelAttribute("SignupRequest") SignupRequest request, BindingResult result, Model model) {
+        try {
+            userService.signupUser(request);
+            model.addAttribute("successMessage", "User registered successfully!");
+            return "signup";
+        } catch (RuntimeException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "signup";
+        }
+    }
+}
