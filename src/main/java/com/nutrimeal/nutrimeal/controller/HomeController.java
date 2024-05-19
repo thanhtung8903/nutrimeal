f023828120695394ed56c20d048abdcd1ec66dc3
unknown <tuanduvip1@gmail.com>
2024-05-19 23:33:13 +0700

    branch du

--- src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.nutrimeal.nutrimeal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -11,4 +13,9 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/xinchaodungua/{name}")
+    public String dunua(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "Beoo";
+    }
 }
