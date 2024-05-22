3dbb46916fc5ac57e2ef259f79845716e13d7edf
GIA KHANH <giakhanh27403@gmail.com>
2024-05-23 00:08:51 +0700

    update address crud

--- src/main/java/com/nutrimeal/nutrimeal/service/AddressService.java
+++ src/main/java/com/nutrimeal/nutrimeal/service/AddressService.java
@@ -6,7 +6,6 @@ import com.nutrimeal.nutrimeal.model.User;
 import com.nutrimeal.nutrimeal.repository.AddressRepository;
 import com.nutrimeal.nutrimeal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,9 +17,16 @@ public class AddressService {
     private final UserRepository userRepository;
     private final AddressRepository addressRepository;
 
+
     public void saveAddress(Address address, String name) {
         User user = userRepository.findByUsername(name).orElseThrow(() -> new RuntimeException("User not found"));
         address.setUser(user);
+        if (address.getDefaultAddress()) {
+            for (Address address1 :addressRepository.findAllByUser(user).orElseThrow(() -> new RuntimeException("Address not found"))) {
+                address1.setDefaultAddress(false);
+                addressRepository.save(address1);
+            }
+        }
         addressRepository.save(address);
     }
 
