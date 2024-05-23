31b995b703aa34891feb368b9cc0ff428bf65415
GIA KHANH <giakhanh27403@gmail.com>
2024-05-23 10:31:55 +0700

    add combo data

--- src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
@@ -1,19 +1,25 @@
 package com.nutrimeal.nutrimeal.controller;
 
+import com.nutrimeal.nutrimeal.model.Combo;
+import com.nutrimeal.nutrimeal.service.ComboService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final HttpSession httpSession;
+    private final ComboService comboService;
 
     @GetMapping(value = {"/", "/home"})
     public String home(Model model) {
+        List<Combo> comboList = comboService.getAllCombos();
+        model.addAttribute("comboList", comboList);
         return "home";
     }
 
@@ -29,6 +35,8 @@ public class HomeController {
 
     @GetMapping("/combo")
     public String combo(Model model) {
+        List<Combo> comboList = comboService.getAllCombos();
+        model.addAttribute("comboList", comboList);
         return "home/combo";
     }
 }
---
+++ src/main/java/com/nutrimeal/nutrimeal/service/ComboService.java
@@ -0,0 +1,46 @@
+package com.nutrimeal.nutrimeal.service;
+
+
+import com.nutrimeal.nutrimeal.model.Combo;
+import com.nutrimeal.nutrimeal.model.ComboType;
+import com.nutrimeal.nutrimeal.repository.ComboRepository;
+import com.nutrimeal.nutrimeal.repository.ComboTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ComboService {
+    private final ComboRepository comboRepository;
+    private final ComboTypeRepository comboTypeRepository;
+
+    public List<Combo> getAllCombos() {
+        return comboRepository.findAll();
+    }
+
+    public Combo getComboById(int id) {
+        return comboRepository.findById(id).orElse(null);
+    }
+
+    public ComboType getComboTypeById(int id) {
+        return comboTypeRepository.findById(id).orElse(null);
+    }
+
+    public Combo addCombo(Combo combo) {
+        return comboRepository.save(combo);
+    }
+
+    public ComboType addComboType(ComboType comboType) {
+        return comboTypeRepository.save(comboType);
+    }
+
+    public void deleteCombo(int id) {
+        comboRepository.deleteById(id);
+    }
+
+    public void deleteComboType(int id) {
+        comboTypeRepository.deleteById(id);
+    }
+}
