d7c880a277bea9ea901ed246e4d4150860d72d57
unknown <tuanduvip1@gmail.com>
2024-05-23 11:29:55 +0700

    fixed menu

--- src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/HomeController.java
@@ -13,8 +13,8 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/headermana/{name}")
+    @GetMapping("/menu/{name}")
     public String dunua(@PathVariable String name, Model model) {
-        return "orderManagement";
+        return "Menu";
     }
 }
