d7f961b949ab6beb0705375bc98ca93e68c86bc7
unknown <tuanduvip1@gmail.com>
2024-06-25 23:24:48 +0700

    fixed being Delivery

---
+++ src/main/java/com/nutrimeal/nutrimeal/controller/shipper/Demo.java
@@ -0,0 +1,14 @@
+package com.nutrimeal.nutrimeal.controller.shipper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class Demo {
+    @GetMapping("/beingDelivery")
+    public String beingDelivery() {
+        return "Shipper/beingDelivery";
+    }
+}
