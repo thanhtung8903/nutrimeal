8b1777de5099f2125aa63fe6d3c578dff0fbad59
unknown <tuanduvip1@gmail.com>
2024-06-26 02:56:12 +0700

    fixed beingDelivery, orderNotYetDelivery, sidebarShipper

--- src/main/java/com/nutrimeal/nutrimeal/controller/shipper/Demo.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/shipper/Demo.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Demo {
     @GetMapping("/beingDelivery")
     public String beingDelivery() {
-        return "Shipper/beingDelivery";
+        return "Shipper/sidebarShipper";
     }
 }
