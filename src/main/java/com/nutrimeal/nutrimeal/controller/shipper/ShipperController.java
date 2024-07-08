72aab353d9067161c708f1676f6beb69d2734b6e
kly102 <kly11022003@gmail.com>
2024-07-09 01:59:40 +0700

    update dashboardShipper

--- src/main/java/com/nutrimeal/nutrimeal/controller/shipper/ShipperController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/shipper/ShipperController.java
@@ -3,8 +3,6 @@ package com.nutrimeal.nutrimeal.controller.shipper;
 import com.nutrimeal.nutrimeal.model.User;
 import com.nutrimeal.nutrimeal.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -48,6 +46,12 @@ public class ShipperController {
         return "shipper/orderDeliveryFail";
     }
 
+    @GetMapping("/dashboardShipper")
+    public String viewDashboardShipper(Model model, Principal principal) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "shipper/dashboardShipper";
+    }
 
 
 }
