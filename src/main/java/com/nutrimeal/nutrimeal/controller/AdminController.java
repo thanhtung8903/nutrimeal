ab5c7ece9ebd7e9a909a06e39db49a1eeecf9835
kly102 <kly11022003@gmail.com>
2024-07-10 11:47:35 +0700

    update dashboardAdmin

--- src/main/java/com/nutrimeal/nutrimeal/controller/AdminController.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/AdminController.java
@@ -1,12 +1,24 @@
 package com.nutrimeal.nutrimeal.controller;
 
+import com.nutrimeal.nutrimeal.model.User;
+import com.nutrimeal.nutrimeal.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
-    
+    private final UserService userService;
+
+    @GetMapping("/dashboard")
+    public String customer(Model model,  Principal principal) {
+        model.addAttribute("listUser", userService.getAllUsers());
+        return "dashboardAdmin";
+    }
 }
