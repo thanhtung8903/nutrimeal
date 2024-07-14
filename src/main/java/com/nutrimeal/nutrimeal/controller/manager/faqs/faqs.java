9c1f857f147bf5943b47154eed620df2472db4ce
unknown <tuanduvip1@gmail.com>
2024-07-15 01:25:29 +0700

    management faqs

---
+++ src/main/java/com/nutrimeal/nutrimeal/controller/manager/faqs/faqs.java
@@ -0,0 +1,14 @@
+package com.nutrimeal.nutrimeal.controller.manager.faqs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/manager")
+@RequiredArgsConstructor
+public class faqs {
+    @GetMapping("/faqs")
+    public String faqs() {
+        return "manager/faqs/faqs";
+    }
+}
