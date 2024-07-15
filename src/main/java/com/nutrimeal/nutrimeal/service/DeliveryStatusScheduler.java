6a4f2ef47d2a41ab045645c00020ed38e20b6b15
GIA KHANH <giakhanh27403@gmail.com>
2024-07-16 00:13:05 +0700

    update: change Date to LocalDate to compare specific date

--- src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
+++ src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
@@ -16,18 +17,6 @@ public class DeliveryStatusScheduler {
 
     private final DeliveryService deliveryService;
 
-//    @Scheduled(cron = "0 0 0 * * ?")
-//    @Transactional
-//    public void updateDeliveryStatus() {
-//        List<Delivery> deliveries = deliveryService.findDeliveriesByDeliveryStatusToUpdateStatus("NOT_DELIVERED");
-//        Date today = new Date();
-//        for (Delivery delivery : deliveries) {
-//            if (delivery.getDeliveryDate().before(today)) {
-//                delivery.setDeliveryStatus("DELIVERY_FAILED");
-//                deliveryService.save(delivery);
-//            }
-//        }
-//    }
 @Scheduled(cron = "0 0 0 * * ?")
 @Transactional
 public void updateDeliveryStatus() {
@@ -37,9 +26,10 @@ public void updateDeliveryStatus() {
     // Phương thức bổ sung để kiểm tra trực tiếp
     public void updateStatus() {
         List<Delivery> deliveries = deliveryService.findDeliveriesByDeliveryStatusToUpdateStatus("NOT_DELIVERED");
-        Date today = new Date();
+        LocalDate today = LocalDate.now();
         for (Delivery delivery : deliveries) {
-            if (delivery.getDeliveryDate().before(today)) {
+            LocalDate deliveryDate = delivery.getDeliveryDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            if (deliveryDate.isBefore(today)) {
                 delivery.setDeliveryStatus("DELIVERY_FAILED");
                 deliveryService.save(delivery);
             }
