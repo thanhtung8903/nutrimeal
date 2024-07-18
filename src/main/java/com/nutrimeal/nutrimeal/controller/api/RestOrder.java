b06de28312e40591f42756df8b9c6495a05c784a
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-07-18 16:43:42 +0700

    feat: manager cancel order

--- src/main/java/com/nutrimeal/nutrimeal/controller/api/RestOrder.java
+++ src/main/java/com/nutrimeal/nutrimeal/controller/api/RestOrder.java
@@ -2,7 +2,11 @@ package com.nutrimeal.nutrimeal.controller.api;
 
 import com.nutrimeal.nutrimeal.dto.response.OrderDetailResponse;
 import com.nutrimeal.nutrimeal.dto.response.OrderResponse;
+import com.nutrimeal.nutrimeal.model.Delivery;
+import com.nutrimeal.nutrimeal.model.DeliveryStatus;
 import com.nutrimeal.nutrimeal.model.Order;
+import com.nutrimeal.nutrimeal.model.OrderStatus;
+import com.nutrimeal.nutrimeal.service.DeliveryService;
 import com.nutrimeal.nutrimeal.service.OrderDetailService;
 import com.nutrimeal.nutrimeal.service.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +22,7 @@ public class RestOrder {
 
     private final OrderService orderService;
     private final OrderDetailService orderDetailService;
+    private final DeliveryService deliveryService;
 
     @GetMapping()
     public ResponseEntity<List<OrderResponse>> getAllOrders(@RequestParam(required = false) String status) {
@@ -44,5 +49,23 @@ public class RestOrder {
         Order order = orderService.getOrderById(orderId);
         List<OrderDetailResponse> orders = orderDetailService.findOrderDetailByOrder(order);
         return ResponseEntity.ok(orders);
+    }
+
+    @PostMapping("/{orderId}/cancel")
+    public ResponseEntity<OrderResponse> cancelOrder(@PathVariable int orderId) {
+        Order order = orderService.getOrderById(orderId);
+        order.setOrderStatus(OrderStatus.CANCELLED);
+
+        List<Delivery> deliveries = deliveryService.findDeliveriesByOrder(order);
+
+        for (Delivery delivery : deliveries) {
+            delivery.setDeliveryStatus(DeliveryStatus.CANCELLED.name());
+            deliveryService.save(delivery);
+        }
+
+        orderService.save(order);
+
+        OrderResponse orderResponse = orderService.findOrderById(order);
+        return ResponseEntity.ok(orderResponse);
     }
 }
