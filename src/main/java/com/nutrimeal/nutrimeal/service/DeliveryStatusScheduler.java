51c464c9b7ccd1d68d23389a9f20b95c388032b5
unknown <tuanduvip1@gmail.com>
2024-07-28 02:03:52 +0700

    add DeliveryStatusScheduler.java

--- src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
+++ src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
 @Component
