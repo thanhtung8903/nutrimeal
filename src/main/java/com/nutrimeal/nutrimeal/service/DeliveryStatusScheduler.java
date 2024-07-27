0a69a8aac257fb675ec1dcd6578d1e2fb14c9515
thanhtung8903 <nguyenthanhtungclc@gmail.com>
2024-07-27 17:01:08 +0700

    update scheduler

--- src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
+++ src/main/java/com/nutrimeal/nutrimeal/service/DeliveryStatusScheduler.java
@@ -3,13 +3,14 @@ package com.nutrimeal.nutrimeal.service;
 import com.nutrimeal.nutrimeal.model.Delivery;
 import com.nutrimeal.nutrimeal.model.DeliveryStatus;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
 @Component
@@ -18,17 +19,23 @@ public class DeliveryStatusScheduler {
 
     private final DeliveryService deliveryService;
 
-@Scheduled(cron = "0 0 0 * * ?")
-@Transactional
-public void updateDeliveryStatus() {
-    updateStatus();
-}
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void updateDeliveryStatusAtMidnight() {
+        updateStatus();
+    }
 
-    public void updateStatus() {
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
+    public void updateDeliveryStatusOnStartup() {
+        updateStatus();
+    }
+
+    private void updateStatus() {
         List<Delivery> deliveries = deliveryService.findDeliveriesByDeliveryStatusToUpdateStatus(DeliveryStatus.NOT_DELIVERED.name());
         LocalDate today = LocalDate.now();
         for (Delivery delivery : deliveries) {
-            LocalDate deliveryDate = delivery.getDeliveryDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate deliveryDate = ((java.sql.Date) delivery.getDeliveryDate()).toLocalDate();
             if (deliveryDate.isBefore(today)) {
                 delivery.setDeliveryStatus(DeliveryStatus.DELIVERY_FAILED.name());
                 deliveryService.save(delivery);
